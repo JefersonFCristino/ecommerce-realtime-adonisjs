87faa39b45b363e43d8e098473d78b5834d255b8
Jeferson Fernandes <jfcristino.fernandes@gmail.com>
2020-03-11 16:15:10 -0300

    criação/migration de relacionamento users/images

---
+++ database/migrations/1583952410783_user_image_fk_schema.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+// Estamos criando esse relacionamento aqui pois na hora que rodarmos a migration de 'users' a tabela de 'images' ainda não vai existir dando assim um erro
+
+class UserImageFkSchema extends Schema {
+  up () {
+    this.table('users', (table) => {
+      // alter table
+      table.foreign('image_id').references('id').inTable('images').onDelete('cascade')
+    })
+  }
+
+  down () {
+    this.table('users', (table) => {
+      // reverse alternations
+      table.dropForeing('image_id')
+    })
+  }
+}
+
+module.exports = UserImageFkSchema
