9bcaf3683c6df6de7c366fd95e4acae9299a2ea6
Jeferson <jfcristino.fernandes@gmail.com>
2020-03-12 16:40:25 -0300

    course: relacionamentos no Model de user

--- app/Models/User.js
+++ app/Models/User.js
@@ -7,21 +7,33 @@ const Model = use('Model')
 const Hash = use('Hash')
 
 class User extends Model {
-  static boot () {
+  static boot() {
     super.boot()
 
     /**
      * A hook to hash the user password before saving
      * it to the database.
      */
-    this.addHook('beforeSave', async (userInstance) => {
+    this.addHook('beforeSave', async userInstance => {
+      // Caso mudemos a senha o lucid detecta que esse campo foi modificado e cria a mesma propriedade (nesse caso, 'password') só que dentro de uma outra propriedade chamada dirty. dirty.password = password modificado que está dentro de dirty (caso exista execulta o if)
+
       if (userInstance.dirty.password) {
         userInstance.password = await Hash.make(userInstance.password)
       }
     })
   }
 
-  static get traits () {
+  /**
+   *  Oculta os campos definidos no retorno das queries no DB
+   */
+
+  static get hidden() {
+    return ['password']
+  }
+
+  // trait é basicamente um bloco de código que estamos trazendo para o nosso model
+
+  static get traits() {
     return [
       '@provider:Adonis/Acl/HasRole',
       '@provider:Adonis/Acl/HasPermission'
@@ -38,8 +50,18 @@ class User extends Model {
    *
    * @return {Object}
    */
-  tokens () {
+  tokens() {
     return this.hasMany('App/Models/Token')
+  }
+
+  // this referencia o Model ou seja, nesse caso, o Model user (this.belongsTo = user.belongsTo)
+
+  image() {
+    return this.belongsTo('App/Models/Image')
+  }
+
+  coupon() {
+    return this.belongsToMany('App/Models/Coupon')
   }
 }
 
