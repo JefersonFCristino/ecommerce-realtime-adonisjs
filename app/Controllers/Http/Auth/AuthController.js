bd2bc266a1962e520fb7572c58a6fef4a0987bd5
Jeferson <jfcristino.fernandes@gmail.com>
2020-03-16 18:23:33 -0300

    course: criando a autenticação do sistema

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -1,31 +1,74 @@
 'use strict'
 
+const Database = use('Database')
+const User = use('App/Models/User')
+const Role = use('Role')
 class AuthController {
-  async register({ req, res }) {
+  async register({ request, response }) {
+    const trx = await Database.beginTransaction()
+
+    try {
+      const { name, surname, email, password } = request.all()
+      const user = await User.create({ name, surname, email, password }, trx)
+
+      const userRole = await Role.findBy('slug', 'client')
+      await user.roles().attach([userRole.id], null, trx)
+
+      await trx.commit()
+      return response.status(201).send({ data: user })
+    } catch (error) {
+      await trx.rollback()
+      return response
+        .status(400)
+        .send({ message: 'Erro ao realizar cadastro!' })
+    }
+  }
+
+  async login({ request, response, auth }) {
+    const { email, password } = request.all()
+
+    let token = await auth.withRefreshToken().attempt(email, password)
+
+    return response.send({ token })
+  }
+
+  async refresh({ request, response, auth }) {
+    // Vai gerar um novo token de autenticação valido a partir de um refresh token que foi gerado com o nosso login acima
+
+    let refresh_token = request.input('refresh_token')
+
+    if (!refresh_token) {
+      refresh_token = request.header('refresh_token')
+    }
+
+    const rToken = await auth
+      .newRefreshToken()
+      .generateForRefreshToken(refresh_token)
+
+    return response.send({ data: rToken })
+  }
+
+  async logout({ request, response, auth }) {
+    let refresh_token = request.input('refresh_token')
+
+    if (!refresh_token) {
+      refresh_token = request.header('refresh_token')
+    }
+
+    await auth.authenticator('jwt').revokeTokens([refresh_token], true) // true vai deletar esse token da base, como se ele nunca tivesse existido
+
+    return response.status(204).send({})
+  }
+
+  async forgot({ request, response }) {
     //
   }
 
-  async login({ req, res, auth }) {
+  async remember({ request, response }) {
     //
   }
 
-  async refresh({ req, res, auth }) {
-    //
-  }
-
-  async logout({ req, res, auth }) {
-    //
-  }
-
-  async forgot({ req, res }) {
-    //
-  }
-
-  async remember({ req, res }) {
-    //
-  }
-
-  async reset({ req, res }) {
+  async reset({ request, response }) {
     //
   }
 }
